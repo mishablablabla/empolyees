e9dfb8365088adb1813897ced89c276d3b0341dc
mishablablabla <mzuew2006@gmail.com>
2025-03-23 23:46:09 +0100

    promote functionality

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,15 +1,24 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 
-import './employees-list.css';
+import "./employees-list.css";
 
-const EmployeesList = () => {
-    return (
-        <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-        </ul>
-    )
-}
+const EmployeesList = ({ data }) => {
+  const elements = data.map((item) => {
+    const { id, ...itemProps } = item;
+
+    return <EmployeesListItem key={item.id} {...itemProps} />;
+  });
+
+  return (
+    <ul className="app-list list-group">
+      <h6>
+        * In case you wanna give a promote to an employee, click on his/her
+        name.
+      </h6>
+
+      {elements}
+    </ul>
+  );
+};
 
 export default EmployeesList;
